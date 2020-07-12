3aa8fc44954eb458e641c12a209f3cea180733ca
Lee Jae-Hyung <user@DESKTOP-C34L5NL>
2020-07-12 16:32:14 +0900

    loginpage end, controller, model create

---
+++ src/com/kh/snackking/filter/EncryptFilter.java
@@ -0,0 +1,60 @@
+package com.kh.snackking.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import com.kh.snackking.wrapper.LoginWrapper;
+
+
+/**
+ * Servlet Filter implementation class EncryptFilter
+ */
+@WebFilter("*.us")
+public class EncryptFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncryptFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		
+		HttpServletRequest hRequest = (HttpServletRequest) request;
+		
+		LoginWrapper lw = new LoginWrapper(hRequest);
+		
+		System.out.println("비밀번호 암호화 완료!");
+		// pass the request along the filter chain
+		chain.doFilter(lw, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
