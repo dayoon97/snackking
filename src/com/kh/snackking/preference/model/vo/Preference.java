7b0a658211543a89633ea92acba597bc017a747d
rlaqh <rlaqh@LAPTOP-91DKG1HL>
2020-07-13 17:47:55 +0900

    fdsq
    수정

--- src/com/kh/snackking/preference/model/vo/Preference.java
+++ src/com/kh/snackking/preference/model/vo/Preference.java
@@ -1,22 +1,18 @@
 package com.kh.snackking.preference.model.vo;
 
 public class Preference {
-	private int userNo; 		//유저 아이디
-	private int pNo;			//선호도 아이디
-	private int pBudget;		//예산
-	private int pPersonnel;		//인원수
-	private String pAge;		//나이
-	private String pGender;		//성별
-	private String pPtnameName;	//상품 종류
-	private String pFlavorName;	//맛 종류
-	private String pIncenseName;//향 종류
-	private String pIncense; 	//기타 향
-	private String pAlName;		//알레르기 종류
-	private String pStyle; 		//구성 스타일
-	private String pOffer;		//제공 타입
-	private String pEquipment;	//설비
-	private String pEquipment2;	//기타 설비
-	private String pPacking;	//포장
+	private int userNo;       //유저 번호
+	private int preNo;         //선호도 아이디
+	private int preBudget;      //예산
+	private int prePersonnel;      //인원수
+	private String preAge;      //나이
+	private String preProductTypes;   //상품 종류
+	private String preTaste; //맛 종류
+	private String preFlavor;//향 종류
+	private String preEtcFlavor;    //기타 향
+	private String preAlName;      //알레르기 종류
+	private String preStyle;       //구성 스타일
+	private String preEquipment;   //수요설비
 	
 	
 	public Preference() {}
