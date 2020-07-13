c9f01a12e9cc3f95a8676611648eb73184edd2f2
rlaqh <rlaqh@LAPTOP-91DKG1HL>
2020-07-13 15:14:12 +0900

    Preference
    bohun

--- src/com/kh/snackking/preference/model/vo/Preference.java
+++ src/com/kh/snackking/preference/model/vo/Preference.java
@@ -1,7 +1,22 @@
 package com.kh.snackking.preference.model.vo;
 
 public class Preference {
-	private int userNo; //유저 아이디
-	private int pNo;
-	private int pBudget;
+	private int userNo; 		//유저 아이디
+	private int pNo;			//선호도 아이디
+	private int pBudget;		//예산
+	private int pPersonnel;		//인원수
+	private String pAge;		//나이
+	private String pGender;		//성별
+	private String pPtnameName;	//상품 종류
+	private String pFlavorName;	//맛 종류
+	private String pIncenseName;//향 종류
+	private String pIncense; 	//기타 향
+	private String pAlName;		//알레르기 종류
+	private String pStyle; 		//구성 스타일
+	private String pOffer;		//제공 타입
+	private String pEquipment;	//설비
+	private String pEquipment2;	//기타 설비
+	private String pPacking;	//포장
+	
+	public Preference() {}
 }
