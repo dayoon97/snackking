29cec08d93ad927be5659374da5991c80e4c8556
JENNY <JENNY@DESKTOP-AFONGJ0>
2020-07-13 21:43:44 +0900

    설비할꺼얌..

---
+++ src/com/kh/snackking/equipment/model/vo/Equipment.java
@@ -0,0 +1,14 @@
+package com.kh.snackking.equipment.model.vo;
+
+public class Equipment {
+	
+	private int eCode;
+	private String eType;
+	private String eName;
+	private int eCount;
+    private int possible;
+    private String eUse;
+ 
+	
+	
+}
